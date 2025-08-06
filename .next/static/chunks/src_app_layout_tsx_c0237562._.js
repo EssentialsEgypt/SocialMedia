8b010b1f05c1e6978be3cf101bf658164bc4d861
Hamza <hamza@example.com>
2025-08-06 06:47:05 +0300

    Remove build artifacts and clean repository for deployment

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__dc687cf0._.css",
-  "static/chunks/node_modules_f73d8e02._.js",
-  "static/chunks/src_8c19905b._.js"
-],
-    source: "dynamic"
-});
